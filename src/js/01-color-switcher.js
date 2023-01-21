3d74f4c052a1742be2d5f8eec6bf63c3a2bd1fb6
AliceChpmnk <alisablog50@gmail.com>
2023-01-21 09:47:18 +0200

    color-switcher ready

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    body: document.querySelector('body'),
+}
 
+let intervalID = null;
+const INTERVAL = 1000;
+
+refs.startBtn.addEventListener('click', onStartBtnClick);
+refs.stopBtn.addEventListener('click', onStopBtnClick);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onStartBtnClick() {
+    intervalID = setInterval(() => {
+        const currentColor = getRandomHexColor();
+        refs.body.style.backgroundColor = currentColor;
+    }, INTERVAL);
+    refs.startBtn.removeEventListener('click', onStartBtnClick);
+}
+
+function onStopBtnClick() {
+    clearInterval(intervalID);
+    refs.startBtn.addEventListener('click', onStartBtnClick);
+}
