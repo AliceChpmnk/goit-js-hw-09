da715dad47f370c28a38b120ea15cbd6175e7bc6
AliceChpmnk <alisablog50@gmail.com>
2023-01-23 21:56:26 +0200

    promises ready

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,39 @@
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const refs = {
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+  form: document.querySelector('.form'),
+}
+
+refs.form.addEventListener('submit', event => {
+  event.preventDefault();
+  let delay = Number(refs.delay.value);
+  for (let i = 1; i <= Number(refs.amount.value); i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+    Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+    Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    
+    delay += Number(refs.step.value);
+  }
+});
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position: position, delay: delay});
+      } else {
+        reject({position: position, delay: delay});
+      }
+    }, delay);
+  });
+
+  return promise;
 }
