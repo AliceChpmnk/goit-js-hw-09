bdb5fa0bc2e8f6bb6bdf323407285d9b7cbe5b56
AliceChpmnk <110929937+AliceChpmnk@users.noreply.github.com>
2023-01-24 20:16:48 +0000

    Deploying to gh-pages from @ AliceChpmnk/goit-js-hw-09@35d6788d7ea1489ea7aa0fd2c67c7f786db3ae53 🚀

--- 01-color-switcher.f2ac5f3b.js
+++
@@ -1,2 +0,0 @@
-!function(){var t={startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]"),body:document.querySelector("body")},n=null;function e(){n=setInterval((function(){var n="#".concat(Math.floor(16777215*Math.random()).toString(16));t.body.style.backgroundColor=n}),1e3),t.startBtn.removeEventListener("click",e)}t.startBtn.addEventListener("click",e),t.stopBtn.addEventListener("click",(function(){clearInterval(n),t.startBtn.addEventListener("click",e)}))}();
-//# sourceMappingURL=01-color-switcher.f2ac5f3b.js.map
